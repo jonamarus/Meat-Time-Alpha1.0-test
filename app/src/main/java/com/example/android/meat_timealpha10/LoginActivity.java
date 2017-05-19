c8482ef47499546f93c008772e1b6eb5fe9b10b9
Jonathan Marboux <jonathan.marboux@telenet.be>
2017-05-19 14:10:55 +0200

    Eindelijk gelukt om een dialogue aan te maken

--- app/src/main/java/com/example/android/meat_timealpha10/LoginActivity.java
+++ app/src/main/java/com/example/android/meat_timealpha10/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.android.meat_timealpha10;
 
+import android.app.Activity;
 import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
@@ -9,38 +12,45 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
-public class LoginActivity extends FragmentActivity {
+public class LoginActivity extends Activity {
 
-    TextView pwrecovery;
+    Button pwrecovery;
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-
-        pwrecovery = (TextView) findViewById(R.id.pwrecovery);
-
-    pwrecovery.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            Toast test = Toast.makeText(getApplicationContext(), "hello", Toast.LENGTH_LONG);
-        }
-    });
-
-
-
         //set up notitle
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         //set up full screen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
+
+         context = this;
+        pwrecovery=(Button) findViewById(R.id.pwrecovery);
+        pwrecovery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(context);
+                builder.setMessage(R.string.dialog_message)
+                        .setTitle(R.string.dialog_title);
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+
+
+
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
